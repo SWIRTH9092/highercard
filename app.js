b7eed044653c6aa8665132d3005b87d47c4a0be6
Your Name <you@example.com>
2022-10-21 07:35:35 -0400

    added draw to basic set of cards

--- app.js
+++ app.js
@@ -4,13 +4,18 @@
 // variable for base url
 const baseURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 
+let deckID = "kutpw83xkk0x"
+
+const shuffleDraw = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`
+
 // function that does movie search
 function colorSearch() {
     // constructing url for request
     const url = `${baseURL}`
-    
+     
+
     // make our request
-    $.ajax(url)
+    $.ajax(shuffleDraw)
     .then((color) => {
         console.log(color)
     })
