93f413cf2ccbfd4e21ffdcba44cedb7fcf33c51d
Your Name <you@example.com>
2022-10-23 00:36:07 -0400

    rendered to screen

--- app.js
+++ app.js
@@ -1,41 +1,70 @@
 //  Game info stored in one object
 
 const gameInfo = {
-        deckStatus: {
-        deckID: "new",
-        remainingCardsInDeck: 52,
-        success: " "
-        },
         stats: { 
             gameWins: 0,
             gameLosses: 0,
             gameTies: 0,
-        },
-        // this is one deck card array for each card drawn
-        //    within this array:
-        //      0 - card code 
-        //      1 - link to card face
-        //      2 - suit
-        //      3 - value
-        //      4 - compare value
-        deckCards: [
-                    []
-                    ],      
+        }
 }
+//   Card [0] - card displayed
+//     0 - compareValue
+//     1 - image link
+//   Card [1] = card face down or face up
+//     0 - compareValue
+//     1 - image link
 
-//work variables
-let workIndex;
+//note:
+let cardValues = [
+    [1, "xxxx"],
+    [2, "yyyy"]
+];
+            
+console.log("Cardvalues", cardValues)
 
-let deckID_API = "new"    // default is new for first call
-//  Array to hold the number of cards
+
+//  Define Jquery  variables
+
+const $nodes = {
+    faceShowing: $(".faceshowing"),
+    compareShowing: $(".compareshowing")
+}
 
 // variable for base url
 const baseURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 
-//default is New
-let deckID = "new"  
+//default is New, first draw is 2
+let deckID = "new" 
+let drawNo = 2 
 
-const shuffleDraw = `https://deckofcardsapi.com/api/deck/${deckID_API}/draw/?count=8`
+const draw = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${drawNo}`
+
+
+// save the card values in array
+function saveCard(arrPos, data) {
+
+cardValues [arrPos][1] = (data.cards[arrPos].image)
+
+// cardValues [arrPos].push(data.cards[arrPos].value)
+
+        switch (data.cards[arrPos].value) {
+                case "ACE":
+                    cardValues[arrPos] [0] = (14);
+                    break;
+                case "KING":
+                    cardValues[arrPos] [0] = (13);
+                    break;
+                case "QUEEN":
+                    cardValues[arrPos] [0] = (12);
+                    break;
+                case "JACK":
+                    cardValues[arrPos] [0] = (11);
+                    break;
+                default:
+                    cardValues[arrPos] [0] = (data.cards [arrPos].value);
+                    break;
+        }
+}
 
 // process Deck Status Data
 function processDeck (data) {
@@ -43,41 +72,36 @@ function processDeck (data) {
     deckID_API = data.deck_id
     console.log("before game Info", gameInfo)
     
-    // save deck stats
-    gameInfo.deckStatus.deckID = data.deck_id
-    gameInfo.deckStatus.remainingCardsInDeck = data.remaining
-    gameInfo.deckStatus.success = data.success
-    
+    // check call stats
+    if (data.success !== true) {
+        console.log("unsuccesful API call")
+        return
+    }
+    console.log(data)
+  
     // save card values
-    gameInfo.deckCards = data.cards.map(function (card) {
-        let compareValue = 0
-        switch (card.value) {
-                case "ACE":
-                    compareValue = 14;
-                    break;
-                case "KING":
-                    compareValue = 13;
-                    break;
-                case "QUEEN":
-                    compareValue = 12;
-                    break;
-                case "JACK":
-                    compareValue = 11;
-                    break;
-                default:
-                    compareValue = card.value  
-                    break;
-        }
-        return[card.code, card.image, card.suit, card.value, compareValue]
-        })
- 
-    console.log("after game Info", gameInfo) 
-}
+        saveCard(0,data)
+        saveCard(1,data)
+        console.log(cardValues)
+        renderCard (0, "face")
+        renderCard (1, "back")
+}   
+
+    function renderCard (cardNo, type) {
+        console.log("1-you are here", cardNo, type, cardValues)
+        if (cardNo === 0) {
+            $nodes.faceShowing.attr("src", cardValues [cardNo] [1]) 
+        } else if (type === "back") {
+            $nodes.compareShowing.attr("src", "/images/cardback.png")       
+        } else {
+            $nodes.compareShowing.attr("src", cardValues[cardNo] [1])     
+        } 
+    }
 
     // function gets a new deck of cards and draws
 function getCardDeck() {
     // make api call
-    $.ajax(shuffleDraw)
+    $.ajax(draw)
     .then(
         (drawData) => {
         console.log(drawData)
@@ -88,4 +112,7 @@ function getCardDeck() {
         })
 } 
 
+    
+
     getCardDeck()
+//renderCard(1,"face")
